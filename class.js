65a27bc9dc1c107c285a2990eececc87a1cd7e2b
Banacount <uniquejmal@gmail.com>
2026-05-16 22:46:56 +0800

    added basic score and blame functionality

--- class.js
+++ class.js
@@ -54,3 +54,25 @@ export class Queue {
         return this.list[0];
     }
 }
+
+export class Button {
+    constructor (rect_base, text) {
+        this.rect = rect_base;
+        this.text = text;
+    }
+
+    display (ctx) {
+        ctx.fillStyle = "#c9c9c9";
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 6;
+        ctx.fillRect(this.rect.x, this.rect.y, this.rect.width, this.rect.height);
+        ctx.strokeRect(this.rect.x, this.rect.y, this.rect.width, this.rect.height);
+
+        ctx.fillStyle = "black";
+        ctx.font = "bold 36px Arial";
+        ctx.strokeStyle = "black";
+        ctx.textAlign = "center";
+
+        ctx.fillText(this.text, this.rect.x + 70, this.rect.y + (this.rect.height - 30));
+    }
+} 
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Vec2, Rect, Queue } from "./class.js";
+import { Vec2, Rect, Queue, Button } from "./class.js";
 
 /*
 Gregg: Explain the main concept and mechanics of the game.
@@ -31,8 +31,15 @@ trees_img.branch.src = "./branch.png";
 
 // Game properties
 let log_queue = new Queue();
+let gameStarted = false;
+let score = 0;
+let control_enabled = false;
+let failed_status = "";
 
 // Times ( for delays )
+let time_elapsed = 0;
+let start_delay = 3;
+let job_delay = 0.6;
 
 // Controls properties
 let screenWidth = 500, screenHeight = 500;
@@ -50,11 +57,52 @@ let log_size = 7;
 let player_rect = new Rect(0, 0, 120, 100);
 let isPlayerRight = false;
 
+// Menu state
+const playBtnRect = new Rect(20, 500, 320, 80);
+const playBtn = new Button(playBtnRect, "PLAY");
+
+// Functions
+const defeat = () => {
+    gameStarted = false;
+}
+const start = () => {
+    score = 0;
+    start_delay = 3;
+    job_delay = 0.6;
+
+    log_queue.list = [];
+    for (let i = 0; i < log_size; i++) {
+        if (i > 0) log_queue.enqueue(Math.floor(Math.random() * 2));
+        else log_queue.enqueue(1);
+    }
+
+    isPlayerRight = false;
+    gameStarted = true;
+}
+
 // Main update method
 const update = (dt) => {
+    time_elapsed += dt;
+
+    if (!gameStarted) {
+        playBtnRect.x = (screenWidth - playBtnRect.width) / 2;
+    }
+
     if (pointer_down && !initial_down) {
         // Put one touched events here
+        initial_down = true;
         
+        // Handle button clicks
+        if (!gameStarted) {
+
+            if (playBtnRect.collidePoint(mouseX, mouseY)) {
+                start();
+                return;
+            }
+        }
+        
+        // Gameplay controls
+        if (!control_enabled) return;        
         let screenCenterX = screenWidth / 2;
 
         player_img.state = player_img.swing;
@@ -67,10 +115,34 @@ const update = (dt) => {
         log_queue.dequeue();
         
         // Detect if player got hit
-        if (log_queue.peak() == 1 && isPlayerRight) console.log("Supposed defeat.");
-        else if (log_queue.peak() == 0 && !isPlayerRight) console.log("Supposed defeat.");
+        if (log_queue.peak() == 1 && isPlayerRight) {
+            defeat();
+            failed_status = "You got hit by trunk! tough luck."
+        }
+        else if (log_queue.peak() == 0 && !isPlayerRight) {
+            defeat();
+            failed_status = "You got hit by trunk! tough luck."
+        }
+        else if (job_delay <= 0){
+            defeat();
+            failed_status = "Too slow. You're fired!"
+        }
+        else {
+            score++;
+            job_delay = 2;
+        }
+    }
 
-        initial_down = true;
+    // Start delay
+    if (gameStarted && start_delay >= 0) {
+        start_delay -= dt;
+        control_enabled = false;
+    }
+    else if (start_delay <= 0) control_enabled = true;
+
+    // Job delay
+    if (gameStarted && job_delay >= 0 && start_delay <= 0) {
+        job_delay -= dt;
     }
 
     if (!pointer_down) {
@@ -84,53 +156,79 @@ const draw = (dt) => {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, screenWidth, screenHeight);
 
-    ctx.fillStyle = "red";
-    // Calculate the center of logs in screen
-    let logCenterX = (screenWidth-log_rect.width) / 2;
+    if (gameStarted) {
+        // Calculate the center of logs in screen
+        let logCenterX = (screenWidth-log_rect.width) / 2;
 
-    // Load the logs and their rects
-    for (let i = 0; i < log_size; i++) {
-        // ( Optional ) to show the order of the logs for debugging.
-        //if (log_queue.list[i] == 1) ctx.fillStyle = "red";
-        //else ctx.fillStyle = "blue";
+        // Load the logs and their rects
+        for (let i = 0; i < log_size; i++) {
+            // ( Optional ) to show the order of the logs for debugging.
+            //if (log_queue.list[i] == 1) ctx.fillStyle = "red";
+            //else ctx.fillStyle = "blue";
 
-        let logBottomY = (screenHeight - (log_rect.height * i)) - log_rect.height;
-        //ctx.fillRect(logCenterX, logBottomY, log_rect.width, log_rect.height);
+            let logBottomY = (screenHeight - (log_rect.height * i)) - log_rect.height;
+            //ctx.fillRect(logCenterX, logBottomY, log_rect.width, log_rect.height);
 
-        if (log_queue.list[i] == 1){
-            ctx.drawImage(trees_img.branch, logCenterX+20, logBottomY, 143.6, 79.2);
-            ctx.drawImage(trees_img.treeType1, logCenterX, logBottomY, log_rect.width, log_rect.height);
+            if (log_queue.list[i] == 1){
+                ctx.drawImage(trees_img.branch, logCenterX+20, logBottomY, 143.6, 79.2); ctx.drawImage(trees_img.treeType1, logCenterX, logBottomY, log_rect.width, log_rect.height);
+            }
+            else {
+                ctx.save();
+                ctx.scale(-1, 1);
+                ctx.drawImage(trees_img.branch, -((logCenterX-log_rect.width) - 60) - 143.6, logBottomY, 143.6, 79.2);
+                ctx.restore();
+
+                ctx.drawImage(trees_img.treeType2, logCenterX, logBottomY, log_rect.width, log_rect.height);
+            }
+        }
+
+        // Calculate the center of X and add a lil bit of offset from the center.
+        let playerCenterX = (screenWidth-player_rect.width) / 2;
+
+        // Player at the bottom
+        player_rect.y = screenHeight - player_rect.height;
+
+        // Draw player stuffs
+        // Set the offset depending on the 'isPlayerRight' variable.
+        if (isPlayerRight) {
+            player_rect.x = playerCenterX + 50;
+
+            ctx.drawImage(player_img.state, player_rect.x, player_rect.y, player_rect.width, player_rect.height);
         }
         else {
-            ctx.save();
+            player_rect.x = playerCenterX - 50;
+            ctx.save()
             ctx.scale(-1, 1);
-            ctx.drawImage(trees_img.branch, -((logCenterX-log_rect.width) - 60) - 143.6, logBottomY, 143.6, 79.2);
+
+            ctx.drawImage(player_img.state, -player_rect.x - player_rect.width, player_rect.y, player_rect.width, player_rect.height);
             ctx.restore();
-
-            ctx.drawImage(trees_img.treeType2, logCenterX, logBottomY, log_rect.width, log_rect.height);
         }
-    }
-
-    // Calculate the center of X and add a lil bit of offset from the center.
-    let playerCenterX = (screenWidth-player_rect.width) / 2;
-
-    // Player at the bottom
-    player_rect.y = screenHeight - player_rect.height;
-
-    // Draw player stuffs
-    // Set the offset depending on the 'isPlayerRight' variable.
-    if (isPlayerRight) {
-        player_rect.x = playerCenterX + 50;
-
-        ctx.drawImage(player_img.state, player_rect.x, player_rect.y, player_rect.width, player_rect.height);
-    }
+    } 
+    // Game Menu
     else {
-        player_rect.x = playerCenterX - 50;
-        ctx.save();
-        ctx.scale(-1, 1);
+        // Render play button
+        playBtn.display(ctx);
 
-        ctx.drawImage(player_img.state, -player_rect.x - player_rect.width, player_rect.y, player_rect.width, player_rect.height);
-        ctx.restore();
+        // Score display
+        ctx.fillStyle = "black";
+        ctx.font = "bold 50px Arial";
+        ctx.textAlign = "center";
+
+        ctx.fillText(`Score: ${score}`, screenWidth/2, 400);
+
+        ctx.fillStyle = "red";
+        ctx.font = "bold 50px Arial";
+        ctx.textAlign = "center";
+
+        ctx.fillText(failed_status, screenWidth/2, 700);
+    }
+
+    if (start_delay >= 0 && gameStarted) {
+        ctx.fillStyle = "red";
+        ctx.font = "bold 120px Arial";
+        ctx.strokeStyle = "black";
+        ctx.textAlign = "center";
+        ctx.fillText(`${Math.trunc(start_delay) + 1}`, screenWidth/2, screenHeight/2);
     }
 }
 
